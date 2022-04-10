caf395d1b8909efc5e1c9917d89feb60ad9c7f6e
rhokkar <cpe2877@students.fhv.at>
2022-04-10 16:48:33 +0200

    - added SessionFactory and Session

--- src/main/java/at/fhv/jazzers/shared/api/RMI_Session.java
+++ src/main/java/at/fhv/jazzers/shared/api/RMI_Session.java
@@ -1,9 +1,5 @@
 package at.fhv.jazzers.shared.api;
 
-import at.fhv.jazzers.shared.api.RMI_CustomerService;
-import at.fhv.jazzers.shared.api.RMI_ProductService;
-import at.fhv.jazzers.shared.api.RMI_SaleService;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
