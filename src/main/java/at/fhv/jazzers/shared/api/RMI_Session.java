f852c443ef84b3886af7594836004f7ef9f3569f
rhokkar <cpe2877@students.fhv.at>
2022-04-10 17:07:22 +0200

    - added username

--- src/main/java/at/fhv/jazzers/shared/api/RMI_Session.java
+++ src/main/java/at/fhv/jazzers/shared/api/RMI_Session.java
@@ -4,6 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RMI_Session extends Remote {
+    String username() throws RemoteException;
     RMI_CustomerService rmi_customerService() throws RemoteException;
     RMI_ProductService rmi_productService() throws RemoteException;
     RMI_SaleService rmi_saleService() throws RemoteException;
