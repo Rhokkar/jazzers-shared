10ee8109b916a49b51d1449459133c9ad336fb79
Barbi <barbaros.teker@gmx.net>
2022-05-05 18:55:29 +0200

    added Date to SaleHistoryEntryOverviewDTO

--- src/main/java/at/fhv/jazzers/shared/dto/SaleHistoryEntryOverviewDTO.java
+++ src/main/java/at/fhv/jazzers/shared/dto/SaleHistoryEntryOverviewDTO.java
@@ -1,6 +1,7 @@
 package at.fhv.jazzers.shared.dto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -10,13 +11,15 @@ public class SaleHistoryEntryOverviewDTO implements Serializable {
     private final String lastName;
     private final double totalPrice;
     private final int quantity;
+    private final LocalDate date;
 
-    public SaleHistoryEntryOverviewDTO(UUID saleId, String firstName, String lastName, double totalPrice, int quantity) {
+    public SaleHistoryEntryOverviewDTO(UUID saleId, String firstName, String lastName, double totalPrice, int quantity, LocalDate date) {
         this.saleId = saleId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.totalPrice = totalPrice;
         this.quantity = quantity;
+        this.date = date;
     }
 
     public UUID getSaleId() {
@@ -39,16 +42,20 @@ public class SaleHistoryEntryOverviewDTO implements Serializable {
         return quantity;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SaleHistoryEntryOverviewDTO that = (SaleHistoryEntryOverviewDTO) o;
-        return Double.compare(that.totalPrice, totalPrice) == 0 && quantity == that.quantity && saleId.equals(that.saleId) && firstName.equals(that.firstName) && lastName.equals(that.lastName);
+        return Double.compare(that.totalPrice, totalPrice) == 0 && quantity == that.quantity && saleId.equals(that.saleId) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && date.equals(that.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(saleId, firstName, lastName, totalPrice, quantity);
+        return Objects.hash(saleId, firstName, lastName, totalPrice, quantity, date);
     }
 }
