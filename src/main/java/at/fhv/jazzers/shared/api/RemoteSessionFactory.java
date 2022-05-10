ed07624b1d42a09c5a42fdcfe2d3c5a434bead6b
rhokkar <cpe2877@students.fhv.at>
2022-05-10 17:11:05 +0200

    - deleted session factory

--- src/main/java/at/fhv/jazzers/shared/api/RemoteSessionFactory.java
+++
@@ -1,8 +0,0 @@
-package at.fhv.jazzers.shared.api;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface RemoteSessionFactory {
-    RemoteSession authenticate(String username, String password);
-}
