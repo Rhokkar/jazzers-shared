97222285ab1c54865b7a59830f894f706f21d308
janikfffhv <janik.fuchsberger@students.fhv.at>
2022-06-20 16:55:05 +0200

    Added download Link

--- src/main/java/at/fhv/jazzers/shared/dto/DigitalProductDTO.java
+++ src/main/java/at/fhv/jazzers/shared/dto/DigitalProductDTO.java
@@ -12,17 +12,20 @@ public class DigitalProductDTO implements Serializable {
     private String genre;
     private double price;
 
+    private String downloadLink;
+
     public DigitalProductDTO() {
 
     }
 
-    public DigitalProductDTO(UUID productId, String title, String interpret, String medium, String genre, double price) {
+    public DigitalProductDTO(UUID productId, String title, String interpret, String medium, String genre, double price, String downloadLink) {
         this.productId = productId;
         this.title = title;
         this.interpret = interpret;
         this.medium = medium;
         this.genre = genre;
         this.price = price;
+        this.downloadLink = downloadLink;
     }
 
     public UUID getProductId() {
@@ -49,16 +52,18 @@ public class DigitalProductDTO implements Serializable {
         return price;
     }
 
+    public String getDownloadLink() { return downloadLink; }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DigitalProductDTO that = (DigitalProductDTO) o;
-        return Double.compare(that.price, price) == 0 && productId.equals(that.productId) && title.equals(that.title) && interpret.equals(that.interpret) && medium.equals(that.medium) && genre.equals(that.genre);
+        return Double.compare(that.price, price) == 0 && productId.equals(that.productId) && downloadLink.equals(that.downloadLink) && title.equals(that.title) && interpret.equals(that.interpret) && medium.equals(that.medium) && genre.equals(that.genre);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(productId, title, interpret, medium, genre, price);
+        return Objects.hash(productId, title, interpret, medium, genre, price, downloadLink);
     }
 }
